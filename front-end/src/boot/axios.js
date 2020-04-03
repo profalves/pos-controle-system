c70fde9de573e6f27245a5160fa4253b6b20251d
Rodrigo Alves <igo.sud@hotmail.com>
2020-04-03 19:26:14 -0300

    initialization

--- front-end/src/boot/axios.js
+++ front-end/src/boot/axios.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://localhost:5001/api/';
+axios.defaults.baseURL = 'https://my-json-server.typicode.com/profalves/pos-controle-system/db';
 // axios.defaults.baseURL = 'https://localhost:5001/api/';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
